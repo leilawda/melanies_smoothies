35eaea708e6c358cdc970a2283e7364b99d579e5
leilawda <145074118+leilawda@users.noreply.github.com>
2025-07-13 13:20:29 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import Pandas
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your smoothi!:cup_with_straw: {st.__version__}")
